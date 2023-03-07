c245a7ccf87aa073a534bce6bfbb958747536b30
Atharva Bhawsar <atharvabhawsar18@gmail.com>
2023-03-07 20:42:45 +0530

    Trash icon added, pencil and trash icon aligned

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,56 +1,60 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
-import styles from './TaskItem.module.css'
-import { CheckIcon, PencilIcon } from '@heroicons/react/24/outline'
+// styles
+import styles from './TaskItem.module.css';
 
-const TaskItem = ({ task }) => {
-    const [isChecked, setIsChecked] = useState(task.checked);
+// Library imports
+import { CheckIcon  } from '@heroicons/react/24/outline';
+import { PencilSquareIcon  } from '@heroicons/react/24/outline';
+import { TrashIcon } from '@heroicons/react/24/outline';
 
-    const handleCheckboxchange = (e) => setIsChecked(!isChecked);
+const TaskItem = ({task}) => {
+  const [isChecked, setIsChecked ] = useState(task.checked);
+
+  const handleCheckboxChange = (e) =>{
+    setIsChecked(!isChecked);
+  }
 
   return (
-      <li className={styles.task}>
-          <div className="task-group">
-              <div className={styles["task-group"]}>
-                  <input
-                      className={styles.checkbox}
-                      type="checkbox"
-                      checked={isChecked}
-                      onChange={handleCheckboxchange}
-                      name={task.name}
-                      id={task.id}
-                  >
-                  </input>
-                  <label
-                      htmlFor={task.id}
-                      className={styles.label}
-                  >
-                      {task.name}
-                      <p className={styles.checkmark}>
-                          <CheckIcon
-                              strokeWidth={5}
-                              width={24}
-                              height={24}
-                          />
-                      </p>
-                  </label>
-              </div>
-                <div className={styles["task-group"]}>
-                  <button
-                      className='btn'
-                      aria-label = {`Update ${task.name} Task`}
-                    //   onClick = {}
-                      >
-                         <PencilIcon
-                              width={24}
-                              height ={24}
-                          />
-                  </button> 
-                </div>
-          </div>
+    <li className={styles.task}>
+      <div className={styles["task-group"]}>
+        <input
+          type="checkbox"
+          className={styles.checkbox}
+          checked={isChecked}
+          onChange={handleCheckboxChange}
+          name={task.name}
+          id={task.id}
+        />
+        <label
+          htmlFor={task.id}
+          className={styles.label}
+        >
+          {task.name}
+          <p className={styles.checkmark}>
+            <CheckIcon strokeWidth={2} width={24} height={24}/>
+          </p>
+        </label>
+      </div>
+      <div className={styles["task-group"]}>
+        <button
+          className='btn'
+          aria-label={`Update ${task.name} Task`}
+          // onClick={}
+        >
+          <PencilSquareIcon width={24} height={24} />
+        </button>
 
+        <button
+          className={`btn ${styles.delete}`}
+          aria-label={`Delete ${task.name} Task`}
+          // onClick={}
+        >
+          <TrashIcon width={24} height={24} />
+        </button>
 
-      </li>
+      </div>
+    </li>
   )
-  }
+}
 export default TaskItem
