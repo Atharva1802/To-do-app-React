3b068f977bb1009f8e5b897b7563c61e21178289
Atharva Bhawsar <atharvabhawsar18@gmail.com>
2023-03-08 23:59:19 +0530

    Save on refresh Successful

--- src/hooks/useLocalStorage.jsx
+++ src/hooks/useLocalStorage.jsx
@@ -1,21 +1,23 @@
 import { useEffect, useState } from "react"
 
 const useLocalStorage = (key, initialValue) => {
+  const [value, setValue] = useState(() => {
     try {
-        const [value, setValue] = useState(() => {
-            const localValue = window.localStorage.getItem(key);       // Gett item key and store to Local storge for reference
-            return localValue ? JSON.parse(localValue) : initialValue  // Only parse when key is found 
-        })        
-    } catch (err) { 
-        console.log(err);
-        return initialValue
+      const localValue = window.localStorage.getItem(key);      // Gett item key and store to Local storge for reference
+      return localValue ? JSON.parse(localValue) : initialValue;    // Only parse when key is found 
+    } catch (err) {
+      console.log(err)
+      return initialValue;
     }
+  })
     // Error may occur in JSON parse or getting the local storage
 
-    useEffect(() => {
-        window.localStorage.setItem(kwy, JSON.stringify(value))
-        //Converts a JavaScript value to a JavaScript Object Notation (JSON) string
-    }, [key, value])    //whenever value changes, tey block updates and sets the new value to initial value
-  return (value, setValue)
+  useEffect(() => {
+      window.localStorage.setItem(key, JSON.stringify(value))
+      //Converts a JavaScript value to a JavaScript Object Notation (JSON) string
+  }, [key, value])     //whenever value changes, tey block updates and sets the new value to initial value
+
+  return [value, setValue]
 }
+
 export default useLocalStorage
