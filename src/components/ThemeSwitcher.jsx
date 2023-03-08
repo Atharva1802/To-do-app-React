22a591e181440f3e34e2904ec29f5b54285e4422
Atharva Bhawsar <atharvabhawsar18@gmail.com>
2023-03-09 00:49:55 +0530

    Skeleton of Slide picker

--- src/components/ThemeSwitcher.jsx
+++ src/components/ThemeSwitcher.jsx
@@ -1,8 +1,31 @@
+import { useState } from 'react'
+
+// import global styles
+import styles from './ThemeSwitcher.module.css'
+
 const ThemeSwitcher = () => {
-  return (
-    <div> 
-        
-    </div>
+  const [isColorPicking, setIsColorPicking] = useState(false)
+  
+    return (
+      <aside
+        className={styles.wrapper}
+        //style
+      > 
+        {
+                isColorPicking
+                    ? (
+                    <>
+                        <button>
+                            
+                        </button>
+                        <input type="range" />
+                            
+                        
+                    </>
+                    )
+                    : ()        
+        }
+    </aside>
   )
 }
 export default ThemeSwitcher
