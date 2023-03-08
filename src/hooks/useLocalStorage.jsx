22e6b1ca7853e779444f65edd6f7e324bf1256dd
Atharva Bhawsar <atharvabhawsar18@gmail.com>
2023-03-08 23:31:19 +0530

    Local storage get value and save it

--- src/hooks/useLocalStorage.jsx
+++ src/hooks/useLocalStorage.jsx
@@ -1,10 +1,10 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 const useLocalStorage = (key, initialValue) => {
     try {
         const [value, setValue] = useState(() => {
-            const localValue = window.localStorage.getItem(key);
-            return localValue ? JSON.parse(localValue) : initialValue  
+            const localValue = window.localStorage.getItem(key);       // Gett item key and store to Local storge for reference
+            return localValue ? JSON.parse(localValue) : initialValue  // Only parse when key is found 
         })        
     } catch (err) { 
         console.log(err);
@@ -12,6 +12,10 @@ const useLocalStorage = (key, initialValue) => {
     }
     // Error may occur in JSON parse or getting the local storage
 
+    useEffect(() => {
+        window.localStorage.setItem(kwy, JSON.stringify(value))
+        //Converts a JavaScript value to a JavaScript Object Notation (JSON) string
+    }, [key, value])    //whenever value changes, tey block updates and sets the new value to initial value
   return (value, setValue)
 }
 export default useLocalStorage
