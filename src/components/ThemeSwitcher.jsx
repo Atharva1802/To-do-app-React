ef26b38e8fd1edda67566228c1c410da45c87c51
Atharva Bhawsar <atharvabhawsar18@gmail.com>
2023-03-09 01:05:37 +0530

    Switch Sun and Moon

--- src/components/ThemeSwitcher.jsx
+++ src/components/ThemeSwitcher.jsx
@@ -3,8 +3,13 @@ import { useState } from 'react'
 // import global styles
 import styles from './ThemeSwitcher.module.css'
 
+// import icons
+import { XMarkIcon, SunIcon, MoonIcon, SwatchIcon } from '@heroicons/react/24/outline'
+
 const ThemeSwitcher = () => {
   const [isColorPicking, setIsColorPicking] = useState(false)
+  const [theme, setTheme] = useState('light')
+  const handleThemeBtn = () => setTheme(theme === "light" ? "dark" : "light")
   
     return (
       <aside
@@ -12,18 +17,36 @@ const ThemeSwitcher = () => {
         //style
       > 
         {
-                isColorPicking
-                    ? (
-                    <>
-                        <button>
-                            
-                        </button>
-                        <input type="range" />
-                            
+            isColorPicking
+                ? (
+                <>
+                    <button>
+                        a            
+                    </button>
+                    <input type="range" />
                         
-                    </>
-                    )
-                    : ()        
+                    
+                </>
+                )
+                : (
+                    <div className={styles.btns}>
+                            <button
+                                className='btn'
+                                aria-label={`change theme to 
+                                ${theme === "light" ? "dark" : "light"} mode`}
+                                role="switch"
+                                onClick={handleThemeBtn}
+                            >
+                            {theme === "dark" ? <SunIcon /> : <MoonIcon />}
+                        </button>
+                            <button
+                                className='btn'
+                                aria-label='Enable color picking mode'
+                            >
+                            <SwatchIcon />
+                        </button>
+                    </div>
+                )        
         }
     </aside>
   )
